310b886e8b7e74a6368509c656f9e53df95d17cd
liuzhengliang <1503600235@qq.com>
2026-08-19 11:35:03 +0800

    feat: 连接本地ollama模型

--- code/chapter7/my_main.py
+++ code/chapter7/my_main.py
@@ -1,7 +1,7 @@
 '''
 Date: 2026-08-19 10:15:10
 LastEditors: liuzhengliang
-LastEditTime: 2026-08-19 10:22:59
+LastEditTime: 2026-08-19 11:33:46
 Description: 
 '''
 from dotenv import load_dotenv
@@ -9,7 +9,15 @@ from my_llm import MyLLM
 
 load_dotenv()
 
-llm=MyLLM(provider="modelscope")
+# llm=MyLLM(provider="modelscope")
+
+# 连接本地模型
+llm=MyLLM(
+    provider="ollama",
+    base_url="http://localhost:11434/v1",
+    api_key="ollama",
+    model="llama3"
+    )
 
 messages = [{"role": "user", "content": "你好，请介绍一下你自己。"}]
 
